569c7911744bba0e5da65b9e9b93dc2c4963ac11
Bars <barsbekclash@gmail.com>
2021-03-11 15:20:58 +0600

    add models and admins register

--- home/webapp/admin.py
+++ home/webapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from webapp.models import Task, Type, Status
 
+# Register your models here.
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'description', 'created_at', 'updated_at', 'status']
+    list_filter = ['type']
+    search_fields = ['summary', 'description']
+    fields = ['id', 'summary', 'description', 'type', 'status', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at', 'id']
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Type)
+admin.site.register(Status)
 # Register your models here.
--- home/webapp/models.py
+++ home/webapp/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+class TimeBase(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Type(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+
+    class Meta:
+        db_table = 'Type'
+        verbose_name = 'Тип'
+        verbose_name_plural = 'Типы'
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Status(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+
+    class Meta:
+        db_table = 'Status'
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Task(TimeBase):
+    summary = models.CharField(max_length=300, null=False, blank=False)
+    description = models.TextField(max_length=3000, null=True, blank=True)
+    status = models.ForeignKey('webapp.Status', on_delete=models.CASCADE, related_name='status', verbose_name='Статус')
+    type = models.ForeignKey('webapp.Type', on_delete=models.CASCADE, related_name='types', verbose_name='Тип')
+
+    class Meta:
+        db_table = 'Task'
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return f'{self.id}. {self.summary}: {self.description}'
+
+
