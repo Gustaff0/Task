85a66dc36d5455768a5ced5539ff3e23e43c2f8e
Bars <barsbekclash@gmail.com>
2021-03-18 15:53:18 +0600

    add base_views.py and modelform

--- home/webapp/forms.py
+++ home/webapp/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from django.forms import widgets
-from webapp.models import Status, Type
+from webapp.models import Status, Type, Task
+#
+# class TaskForm(forms.Form):
+#     summary = forms.CharField(max_length=200, required=True, label='Summary')
+#     description = forms.CharField(max_length=3000, label='Description', required=False, widget=widgets.Textarea)
+#     status = forms.ModelChoiceField(queryset=Status.objects.all())
+#     type = forms.ModelMultipleChoiceField(queryset=Type.objects.all())
 
-class TaskForm(forms.Form):
-    summary = forms.CharField(max_length=200, required=True, label='Summary')
-    description = forms.CharField(max_length=3000, label='Description', required=False, widget=widgets.Textarea)
-    status = forms.ModelChoiceField(queryset=Status.objects.all())
-    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all())
+class TaskForm(forms.ModelForm):
+
+    class Meta:
+        model = Task
+        fields = ('summary', 'description', 'status', 'type')
--- home/webapp/views.py
+++ home/webapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import View, TemplateView, RedirectView
+from django.views.generic import View, TemplateView, RedirectView, FormView
 from django.http import HttpResponseRedirect, HttpResponseNotFound, Http404
 from webapp.models import Task
 from webapp.forms import TaskForm
+from webapp.base_views import FormView as CustomFormView
+from django.urls import reverse
 # Create your views here.
 
 class List_tasks(TemplateView):
@@ -13,50 +15,52 @@ class List_tasks(TemplateView):
         return super().get_context_data(**kwargs)
 
 
-class Create_task(View):
-    def get(self, request, *args, **kwargs):
-        form= TaskForm()
-        return render(request, 'create.html', {'form':form})
-    def post(self, request, *args, **kwargs):
-        form = TaskForm(data=request.POST)
-        if form.is_valid():
-            summary = form.cleaned_data.get('summary')
-            description = form.cleaned_data.get("description")
-            status = form.cleaned_data.get('status')
-            type = form.cleaned_data.get('type')
-            task = Task.objects.create(
-                summary=summary,
-                description=description,
-                status=status
-            )
-            task.type.set(type)
-            return redirect('home')
-        else:
-            return render(request, 'create.html', {'form': form})
+class Create_task(CustomFormView):
+    template_name = 'create.html'
+    form_class = TaskForm
+    redirect_url = 'home'
+
+    def form_valid(self, form):
+        type = form.cleaned_data.pop('type')
+        task = Task()
+        for key, value in form.cleaned_data.items():
+            setattr(task, key, value)
+
+        task.save()
+        task.type.set(type)
+
+        return super().form_valid(form)
 
 
-class Edit_task(View):
-    def get(self, request, *args, **kwargs):
-        task = get_object_or_404(Task, id=kwargs.get('pk'))
-        form = TaskForm(initial={
-            'summary': task.summary,
-            'description': task.description,
-            'status': task.status,
-            'type': task.type.all()
-        })
-        return render(request, 'edit.html', context={'form': form, 'task': task})
-    def post(self, request, *args, **kwargs):
-        task = get_object_or_404(Task, id=kwargs.get('pk'))
-        form = TaskForm(data=request.POST)
-        if form.is_valid():
-            task.summary = form.cleaned_data['summary']
-            task.description = form.cleaned_data['description']
-            task.status = form.cleaned_data['status']
-            task.type.set(form.cleaned_data.get('type'))
-            task.save()
-            return redirect('home')
-        else:
-            return render(request, 'edit.html', context={'form': form, 'task': task})
+class Edit_task(FormView):
+    template_name = 'edit.html'
+    form_class = TaskForm
+
+    def dispatch(self, request, *args, **kwargs):
+        self.task = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = self.task
+        return context
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.task
+        return kwargs
+
+    def form_valid(self, form):
+        self.task = form.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('view', kwargs={'pk': self.task.pk})
+
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        return get_object_or_404(Task, pk=pk)
+
 
 
 class View_task(View):
